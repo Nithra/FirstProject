c2f8d87027f69e9857090c000c108f854330bbed
Nithya <nithyarajainfotech@gmail.com>
2024-03-06 12:46:31 +0530

    empty commit

---
+++ basic.js
@@ -0,0 +1,23 @@
+let person = {
+    name : 'Nithya',
+    age : '33',
+    city : 'Madurai'
+};
+console.log(person);
+function greet(Welcome){
+    console.log('Hai ' + Welcome);
+}
+greet('Welcome to our page, please visit out site and leave us a commen !!!')
+function root(number){
+    return number+number; 
+}
+console.log(root(8));
+
+function valid(){
+alert('Clicking button');
+document.getElementsByClassName('button').value='Submitted'
+}
+// strings
+let myVariable = "Mathematics"
+// String Method
+console.log()
